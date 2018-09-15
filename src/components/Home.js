31a0575cd34c84e1a54cdb182d19a495055b7249
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-09-16 03:17:02 +0530

    Higher Order Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,17 @@ import React from 'react';
 
 const Home = () => {
   return(
-    <div className="container">
+    <div >
+      <div className="container">
+      
       <h2>Home Page
       </h2>
+      </div>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. In dolorem architecto soluta inventore. Reprehenderit nihil accusamus qui autem debitis earum repudiandae quia quae eius perferendis? Unde dicta libero doloremque nam.
 
       </p>
+
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,15 +3,16 @@ import '@material-ui/core'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
-import {Link,NavLink} from 'react-router-dom';
+import {Link, NavLink, withRouter} from 'react-router-dom';
 
 const Navbar = (props) => {
+console.log(props)
   return(
     <div className="container">
        <AppBar position="static" color="primary">
         <Toolbar>
           <Button  color="secondary"><Link to="/">Home</Link> </Button>
-          <Button   color="inherit"><NavLink to="about">About</NavLink></Button>
+          <Button   color="inherit"><NavLink to="/about">About</NavLink></Button>
           <Button   color="inherit"><NavLink to="/Contact">Contact</NavLink></Button>
         </Toolbar>
         
@@ -20,4 +21,4 @@ const Navbar = (props) => {
   )
 }
 
-export default Navbar;
+export default withRouter(Navbar);
